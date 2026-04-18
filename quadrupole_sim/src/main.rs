997a37c8366c766e8c7796166ebd22bd5ecb4dad
Rishaad Khan <radishranger123@gmail.com>
2026-04-18 14:07:21 -0400

    Made a better comment

--- quadrupole_sim/src/main.rs
+++ quadrupole_sim/src/main.rs
@@ -4,7 +4,7 @@ mod physics;
 const PROTON_MASS: f32 = 938.7; // The mass of a proton in MeV/c^2
 const PROTON_CHARGE: f32 = 1.0; // Electric Charge
 const MU0: f32 = 1.0; // Magnetic permeability
-const C_TM: f32 = 299.792_458; // Speed of light c = 1 in Natural Units; conversion factor might be needed
+const C_TM: f32 = 299.792_458; // Speed of light c = 1 in Natural Units; conversion factor might be needed: MeV/Tm
 const IN_TO_M: f32 = 0.0254; // Conversion factor 
 const MM_TO_M: f32 = 1e-3; // Conversion factor
 
