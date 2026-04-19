db7063ce70dd2016b6bc13f7b10e2f6156ad196a
Rishaad Khan <radishranger123@gmail.com>
2026-04-19 02:59:19 -0400

    Finished tracking and changed everything to f64 from f32

--- quadrupole_sim/src/main.rs
+++ quadrupole_sim/src/main.rs
@@ -1,14 +1,14 @@
 mod physics;
 
 /// Constants in terms of Natural Units
-const PROTON_MASS: f32 = 938.7; // The mass of a proton in MeV/c^2
+const PROTON_MASS: f64 = 938.7; // The mass of a proton in MeV/c^2
 
 /// SI Units and Conversions
-const MU0: f32 = 1.256_637_061_4e-6; // Magnetic permeability T*m / A
-const C_TM: f32 = 299.792_458; // Speed of light c = 1 in Natural Units; conversion factor might be needed: MeV/T*m
+const MU0: f64 = 1.256_637_061_4e-6; // Magnetic permeability T*m / A
+const C_TM: f64 = 299.792_458; // Speed of light c = 1 in Natural Units; conversion factor might be needed: MeV/T*m
 
-const IN_TO_M: f32 = 0.0254; // Conversion factor 
-const MM_TO_M: f32 = 1e-3; // Conversion factor
+const IN_TO_M: f64 = 0.0254; // Conversion factor 
+const MM_TO_M: f64 = 1e-3; // Conversion factor
 
 fn main() -> std::io::Result<()> {
     Ok(())
--- quadrupole_sim/src/physics.rs
+++ quadrupole_sim/src/physics.rs
@@ -1,13 +1,13 @@
 #![allow(non_snake_case)]
 use anyhow::Result;
-use ndarray::{Array2, array};
+use ndarray::{Array1, Array2, array};
 
 use crate::{C_TM, MU0, PROTON_MASS};
 
 /// Calculates the beam rigidity (B_rho)
 /// Dimensions: T*m
 /// Parameters: ke_mev [the kinetic energy in MeV]
-pub fn beam_rigidity(ke_mev: f32) -> f32 {
+pub fn beam_rigidity(ke_mev: f64) -> f64 {
     let p = ((ke_mev + PROTON_MASS).powi(2) - PROTON_MASS.powi(2)).sqrt(); // Momentum
 
     p / C_TM
@@ -16,19 +16,19 @@ pub fn beam_rigidity(ke_mev: f32) -> f32 {
 /// Calculates the field gradient
 /// Dimensions: T/m
 /// Parameters: i [current], n [turns], r [radius]
-pub fn field_gradient(i: f32, n: usize, r: f32, mu_r: f32) -> f32 {
-    let ni = (n as f32) * i;
-    let kappa = 1.0 / mu_r;  
+pub fn field_gradient(i: f64, n: usize, r: f64, mu_r: f64) -> f64 {
+    let ni = (n as f64) * i;
+    let kappa = 1.0 / mu_r;
 
     (2.0 * MU0 * ni) / (r.powi(2) * (1.0 + kappa))
 }
 
 /// Calculates the quadrupole transfer matrix
 pub fn quad_transfer_matrix(
-    g: f32,     // Field gradient
-    L: f32,     // Effective length
-    B_rho: f32, // The beam rigidity
-) -> (Array2<f32>, Array2<f32>) {
+    g: f64,     // Field gradient
+    L: f64,     // Effective length
+    B_rho: f64, // The beam rigidity
+) -> (Array2<f64>, Array2<f64>) {
     let k = g / B_rho; // Focusing strength
     let kr = k.abs().sqrt();
 
@@ -66,35 +66,53 @@ pub fn quad_transfer_matrix(
 }
 
 /// Returns a drift matrix
-pub fn drift_matrix(L: f32) -> Array2<f32> {
+pub fn drift_matrix(L: f64) -> Array2<f64> {
     array![[1.0, L], [0.0, 1.0]]
 }
 
-/// The Envelope struct, represents a beam envelope
-struct Envelope {
-    x: Vec<f32>,
-    y: Vec<f32>,
-    z: Vec<f32>,
-    // ...
+fn find_crossovers(arr: &[f64], z: &[f64]) -> Vec<f64> {
+    let mut crossovers = Vec::new();
+    for i in 1..arr.len() {
+        if arr[i - 1] * arr[i] < 0.0 {
+            let frac = arr[i - 1].abs() / (arr[i - 1].abs() + arr[i].abs());
+            crossovers.push(z[i - 1] + frac * (z[i] - z[i - 1]));
+        }
+    }
+    crossovers
 }
 
-impl Envelope {
+/// The envelope tracker struct
+struct Tracker {
+    x:            Vec<f64>,
+    y:            Vec<f64>,
+    z:            Vec<f64>,
+    x_f:          f64,      // final |x| position
+    y_f:          f64,      // final |y| position
+    total_length: f64,
+    q1_end:       f64,
+    q2_start:     f64,
+    q2_end:       f64,
+    x_xover:      Vec<f64>,
+    y_xover:      Vec<f64>,
+    max_env_x:    f64,
+    max_env_y:    f64,
+}
+
+impl Tracker {
+
     /// Track beam envelope through FD doublet.
-    /// Returns an Envelope data structure with z positions, x/y envelopes, region boundaries, crossovers, etc.
+    /// Returns an Tracker data structure with z positions, x/y envelopes, region boundaries, crossovers, etc.
     pub fn track_envelope(
-        bore_m: f32,  // Bore radius in meters
-        L_mag_m: f32, // Magnet length in meters
-        gap_m: f32,   // Gap length in meters
-        drift_m: f32, // Drift length in meters
-        g1: f32,      // The field gradient
-        energy_MeV: f32,
-        x0: f32,
-        xp0: f32,
-        mut n_steps: Option<usize>,
-    ) -> Result<Envelope> {
-        if n_steps == None {
-            n_steps = Some(400); // Default steps is 400
-        }
+        bore_m: f64,  // Bore radius in meters
+        L_mag_m: f64, // Magnet length in meters
+        gap_m: f64,   // Gap length in meters
+        drift_m: f64, // Drift length in meters
+        g1: f64,      // The field gradient
+        energy_MeV: f64,
+        x0: f64,
+        xp0: f64,
+        n_steps: usize,
+    ) -> Result<Tracker> {
 
         let Brho = beam_rigidity(energy_MeV);
         let total_length = L_mag_m + gap_m + L_mag_m + drift_m;
@@ -106,6 +124,63 @@ impl Envelope {
             ("drift", 0.0, drift_m),
         ];
 
-        todo!()
+        let mut x = vec![x0];
+        let mut y = vec![x0];
+        let mut z = vec![0.0];
+
+        let mut x_state: Array1<f64> = array![x0, xp0];
+        let mut y_state: Array1<f64> = array![x0, xp0];
+
+        for (r, g, length) in regions {
+            let n = usize::max((n_steps as f64 * length / total_length) as usize, 4);
+            let dz = length / n as f64 ;
+
+            for _ in 0..n {
+                match r {
+                    "quad" => {
+                        let (Mx, My) = quad_transfer_matrix(g, dz, Brho);
+
+                        x_state = Mx.dot(&x_state);
+                        y_state = My.dot(&y_state);
+                    }
+                    _ => {
+                        x_state = drift_matrix(dz).dot(&x_state);
+                        y_state = drift_matrix(dz).dot(&y_state);
+                    }
+                }
+
+                if let Some(z_s) = z.last() {
+                    z.push(z_s + dz)
+                } else {
+                    eprintln!("Error in z tracking envelope")
+                }
+                x.push(x_state[0]);
+                y.push(y_state[0]);
+            }
+        }
+
+        let x_f = x_state[0].abs();
+        let y_f = y_state[0].abs();
+        
+        let max_env_x = x.iter().map(|v| v.abs()).fold(f64::NEG_INFINITY, f64::max);
+        let max_env_y = y.iter().map(|v| v.abs()).fold(f64::NEG_INFINITY, f64::max);
+        
+        let x_xover = find_crossovers(&x, &z);
+        let y_xover = find_crossovers(&y, &z);
+        
+        Ok(Tracker {
+            x, y, z,
+            x_f,
+            y_f,
+            total_length,
+            q1_end: L_mag_m,
+            q2_start: L_mag_m + gap_m,
+            q2_end: 2.0 * L_mag_m + gap_m,
+            x_xover,
+            y_xover,
+            max_env_x,
+            max_env_y,
+        })
     }
 }
+
